c90e2d510bc22e57b05697365bd50a946db20440
sportshead <32637656+sportshead@users.noreply.github.com>
2020-11-21 12:56:55 +0800

    logging + readme

--- README.md
+++ README.md
@@ -1,1 +1,27 @@
 # webpack-buildinfo
+
+Webpack buildinfo is a Webpack loader that injects build information into code.
+
+## Usage
+
+ES6 Modules:
+
+```javascript
+import buildinfo from "!@sportshead/webpack-buildinfo?gitHashShort&time!";
+```
+
+Node.JS require:
+
+```javascript
+const buildinfo = require("!@sportshead/webpack-buildinfo?gitHashShort&time!");
+```
+
+You can then use the `buildinfo` object like so:
+
+```javascript
+console.log(
+    `MyCoolProject v${buildinfo.gitHashShort} compiled at ${new Date(
+        buildinfo.time
+    ).toISOString()}`
+);
+```
--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,13 @@ import { getOptions } from "loader-utils";
 import webpack from "webpack";
 import os from "os";
 
-export default function loader(this: webpack.loader.LoaderContext) {
+export default function loader(
+    this: webpack.loader.LoaderContext & {
+        getLogger: (pluginName: string) => webpack.Logger;
+    }
+) {
+    const logger = this.getLogger("@sportshead/webpack-buildinfo");
+
     const defaultOptions = {
         esModule: true,
         gitHash: false,
@@ -21,61 +27,84 @@ export default function loader(this: webpack.loader.LoaderContext) {
         npmVersion: false,
         yarnVersion: false,
         argv: false,
+        all: false,
     };
     const options = Object.assign({}, defaultOptions, getOptions(this));
+
+    logger.debug(`rootContext: ${this.rootContext}`);
+    logger.log(`Options: ${options}`);
 
     const callback = this.async()!;
 
     const output: BuildInfo = {};
 
-    if (options.gitHash) {
+    if (options.gitHash || options.all) {
+        logger.debug("Including git hash in output object");
         output.gitHash = execa.sync("git", ["rev-parse", "HEAD"]).stdout.trim();
     }
-    if (options.gitHashShort) {
+    if (options.gitHashShort || options.all) {
+        logger.debug("Including short git hash in output object");
         output.gitHashShort = execa
             .sync("git", ["rev-parse", "--short", "HEAD"])
             .stdout.trim();
     }
-    if (options.time) {
+    if (options.time || options.all) {
+        logger.debug("Including time in output object");
         output.time = Date.now();
     }
-    if (options.platform) {
+    if (options.platform || options.all) {
+        logger.debug("Including platform in output object");
         output.platform = os.platform();
     }
-    if (options.arch) {
+    if (options.arch || options.all) {
+        logger.debug("Including platform arch in output object");
         output.arch = os.arch();
     }
-    if (options.cpus) {
+    if (options.cpus || options.all) {
+        logger.debug("Including cpus in output object");
         output.cpus = os.cpus();
     }
-    if (options.hostname) {
+    if (options.hostname || options.all) {
+        this.emitWarning(
+            `Warning: Your hostname is being built into the program. This may potentially be sensitive. Location of usage: ${this.rootContext}`
+        );
+        logger.debug("Including hostname in output object");
         output.hostname = os.hostname();
     }
-    if (options.networkInterfaces) {
+    if (options.networkInterfaces || options.all) {
+        logger.debug("Including network interfaces in output object");
         output.networkInterfaces = os.networkInterfaces();
     }
-    if (options.freemem) {
+    if (options.freemem || options.all) {
+        logger.debug("Including free memory in output object");
         output.freemem = os.freemem();
     }
-    if (options.totalmem) {
+    if (options.totalmem || options.all) {
+        logger.debug("Including total memory in output object");
         output.totalmem = os.totalmem();
     }
-    if (options.userInfo) {
+    if (options.userInfo || options.all) {
+        logger.debug("Including user info in output object");
         output.userInfo = os.userInfo();
     }
-    if (options.webpackVersion) {
+    if (options.webpackVersion || options.all) {
+        logger.debug("Including webpack version in output object");
         output.webpackVersion = webpack.version;
     }
-    if (options.nodeVersion) {
+    if (options.nodeVersion || options.all) {
+        logger.debug("Including node version in output object");
         output.nodeVersion = process.versions.node;
     }
-    if (options.npmVersion) {
+    if (options.npmVersion || options.all) {
+        logger.debug("Including npm version in output object");
         output.npmVersion = execa.sync("npm", ["--version"]).stdout.trim();
     }
-    if (options.yarnVersion) {
+    if (options.yarnVersion || options.all) {
+        logger.debug("Including yarn version in output object");
         output.yarnVersion = execa.sync("yarn", ["--version"]).stdout.trim();
     }
-    if (options.argv) {
+    if (options.argv || options.all) {
+        logger.debug("Including process argv in output object");
         output.argv = process.argv;
     }
 
